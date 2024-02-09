91554b2ce4d0dfc9380fa5f38d2e808c2a9d215e
Igor Storchevoy <hellsgor@gmail.com>
2024-02-09 19:40:37 +0400

    feat: добавлен компонент video

--- src/js/play-button.js
+++ src/js/play-button.js
@@ -2,12 +2,27 @@ export function playButton() {
   document.querySelectorAll('[data-tmpl-hh-video]')
     .forEach((videoBlock) => {
       const cover = videoBlock.querySelector('[data-tmpl-hh-video-cover]');
-      const frame = videoBlock.querySelector('[data-tmpl-hh-video-frame]');
+      const video = videoBlock.querySelector('[data-tmpl-hh-video-video]');
+      const playButton = videoBlock.querySelector('[data-tmpl-hh-video-play-button]');
 
-      cover.addEventListener('click', () => {
+      const showPlayButton = () => {
+        playButton.classList.remove('tmpl-hh-video__play-button_hidden');
+      }
+
+      const hidePlayButton = () => {
+        playButton.classList.add('tmpl-hh-video__play-button_hidden');
+      }
+
+      const playVideo = () => {
         cover.classList.add('tmpl-hh-video__cover_hidden');
-        frame.classList.remove('tmpl-hh-video__frame_hidden');
-        frame.src += "&autoplay=1";
-      })
+        video.classList.remove('tmpl-hh-video__video_hidden');
+        video.volume = .3;
+        video.play();
+      }
+
+      video.addEventListener('pause', showPlayButton);
+      video.addEventListener('play', hidePlayButton);
+      cover.addEventListener('click', playVideo);
+      playButton.addEventListener('click', playVideo);
     })
 }
